dea0922f09e053b4e7587367bba6991069664676
Evan Turner <evturn@gmail.com>
2015-01-20 18:51:48 -0500

    hangman image rendering from backbone

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -2,6 +2,8 @@ var words = new Words();
 words.fetch({reset: true});
 wordsView = new WordsView({collection: words});
 new LetterInput();
+new HangmanImage();
+
 $(function() {
 
 });
---
+++ public/javascripts/views/hangmanImage.js
@@ -0,0 +1,11 @@
+var HangmanImage = Backbone.View.extend({
+	el: '#hangman-image',
+	template: _.template($('#hangman-image-template').html()),
+	initialize: function() {
+		this.render();
+	},
+	render: function() {
+		this.$el.html(this.template());
+		return this;
+	}
+});
