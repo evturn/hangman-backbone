66355932f2a0c69770e796ee0e61e9cce0fae079
Evan Turner <evturn@gmail.com>
2015-01-22 11:56:41 -0500

    made hangmanStart function and call it upon document ready

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,10 +1,13 @@
-game = new Game();
-new GameView({model: game});
-console.log(game);
-new LetterInput();
-bodyParts = new BodyParts();
-
+function hangmanStart() {
+	game = new Game();
+	new GameView({model: game});
+	console.log(game);
+	new LetterInput();
+	bodyParts = new BodyParts();
+};
 
 $(function() {
-	
+
+	hangmanStart();
+
 });
