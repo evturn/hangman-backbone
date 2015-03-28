b7ed313715030d060013dc2b292cc449c9688c64
Evan Turner <evturn@gmail.com>
2015-03-28 04:17:09 -0400

    letter bank and lives rendering to the top of the page

--- public/javascripts/views/gameView.js
+++ public/javascripts/views/gameView.js
@@ -1,12 +1,18 @@
 var GameView = Backbone.View.extend({
 	el: '#game-state',
   template: _.template($('#game-state-template').html()),
+  livesTemplate: _.template($('#lives-template').html()),
 	initialize: function() {
     this.listenTo(this.model, 'change', this.render);
   },
   render: function() {
     this.$el.empty();
+    this.lives();
     this.$el.append(this.template(this.model.toJSON()));
+    return this;
+  },
+  lives: function() {
+    $('#lives').html(this.livesTemplate(this.model.toJSON()));
     return this;
   },
 });
