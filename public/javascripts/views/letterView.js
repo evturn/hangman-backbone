407f95fe4547a5777a087fd79e54ccbf45f3aecb
Evan Turner <evturn@gmail.com>
2015-03-28 03:44:39 -0400

    label moved to top

--- public/javascripts/views/letterView.js
+++ public/javascripts/views/letterView.js
@@ -1,7 +1,5 @@
 var LetterView = Backbone.View.extend({
 	el: '#letters',
-	tagName: 'li',
-	className: 'letter',
 	template: _.template($('#letter-template').html()),
 	initialize: function() {
 		this.render();
